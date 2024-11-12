887089db691820a225ebfbbfa9a84e3ec899d184
PWD <powderserialgamer07@gmail.com>
2024-11-12 20:47:03 +0100

    Started writing the UI with textual It might seem like it's not much, but that was the result of hours of reading the documentation.

--- test.py
+++ test.py
@@ -1,32 +1,33 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Static
-
-
-class Ips_returned(Static):
-    def compose(self) -> ComposeResult:
-        yield Static("Ips returned:", classes="box", id="ips_returned")
-
-
-class Console(Static):
-    def compose(self) -> ComposeResult:
-        yield Static("Console:", classes="box", id="console")
-
-
-class User_ip_input(Static):
-    def compose(self) -> ComposeResult:
-        yield Static("Enter your choice:", classes="box", id="user_ip_input")
+from textual.widgets import Static, Input, Label
+from textual.containers import Center, VerticalScroll
+ip = None
 
 
 class Discovery(App):
     CSS_PATH = "Tcss/grid_layout.tcss"
 
     def compose(self) -> ComposeResult:
-        yield Ips_returned()
-        yield Console()
-        yield User_ip_input()
+        yield VerticalScroll(id="ips_returned")
+        yield VerticalScroll(id="console")
+        yield Input(placeholder="Enter your choice", id="user_ip_input", type="integer")
+
+    def on_input_submitted(self) -> None:
+        self.choose_ip()
+
+    def choose_ip(self) -> None:
+        text_value = self.query_one(Input).value
+        try:
+            value = int(text_value)
+        except ValueError:
+            self.query_one("#console").mount(Label("Invalid input"))
+            self.query_one("#user_ip_input").value = ""
+            return
+        self.query_one("#console").mount(Label(f"{value} has been chosen"))
+        self.query_one("#user_ip_input").value = ""
 
 
-# Save this script as discovery_app.py and ensure your CSS file is correctly placed in the Tcss folder.
+        # Save this script as discovery_app.py and ensure your CSS file is correctly placed in the Tcss folder.
 if __name__ == "__main__":
     app = Discovery()
     app.run()
